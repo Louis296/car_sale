3f9fe6e0e6428ea7f7a2fc2f453b20f1c930bcc8
Louis296 <1zhaozhiwei@gmail.com>
2022-04-07 12:21:23 +0800

    added: project structure

---
+++ src/main/java/com/louis296/car_sale/CarSaleApplication.java
@@ -0,0 +1,13 @@
+package com.louis296.car_sale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarSaleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarSaleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/louis296/car_sale/CarSaleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.louis296.car_sale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarSaleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
