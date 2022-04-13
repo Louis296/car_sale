726703b5a56059c723cdbf870130c421daad9853
Louis296 <1zhaozhiwei@gmail.com>
2022-04-13 17:17:36 +0800

    added: user DML

---
+++ deploy/DML.sql
@@ -0,0 +1,2 @@
+INSERT INTO car_sale.user (id, user_id, password, user_name, phone, type, create_time, update_time, status) VALUES (1, 'zhaozhiwei@sina.cn', '00dc3922cf5f8c34de15c6ba5c7c8d05', 'louis296', '18645061698', 1, '2022-04-13 16:34:50', '2022-04-13 16:34:50', 1);
+INSERT INTO car_sale.user (id, user_id, password, user_name, phone, type, create_time, update_time, status) VALUES (2, 'zhaozhiwei@gmail.com', '00dc3922cf5f8c34de15c6ba5c7c8d05', 'louis296', '18645061698', 0, '2022-04-13 16:37:53', '2022-04-13 16:37:53', 1);
